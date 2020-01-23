2b88288ecfd8277fa0a22c262fb11a6e1a735079
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-01-23 13:03:59 +0900

    Add title No.3, 4 and 5

---
+++ 3/sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '893c76a263a131ebbf2644986bff1f81520b0cc0ef85522a6e34d1eacc2fa7625a39625872167bbb929461807d1385367ee5a2cd71833278df37070c088ef572'
---
+++ 5/meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'b59189f26b3256de5a8bf47b236d389cfec12ce8c302863a7cda473185ea568c89241c05803328ace27426ca492ba5c59d40e66447f3328fc3caf1414f2de7c3'
